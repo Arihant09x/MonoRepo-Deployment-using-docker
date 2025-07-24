9b61b11f4837e4edbaed5486fb1cb17ef2c1d699
Arihant09x <arihantc677@gmail.com>
2025-07-24 15:32:13 +0530

    complet CD code

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,8 +4,8 @@ export default async function page() {
   const user = await prismaclient.user.findMany();
   return (
     <div>
-      {user.map((users) => (
-        <div>
+      {user.map((users, key) => (
+        <div key={key}>
           <p>Username: {users.username}</p>
           <p>password: {users.password}</p>
         </div>
